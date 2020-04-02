080e3bf06ec6cd44db1be3ea19d3408bb66860ab
Frederick Arand <fredi.arand@gmail.com>
2020-04-02 09:13:35 +0200

    removed obsolete code

--- Wahlergebnisse.py
+++ Wahlergebnisse.py
@@ -11,8 +11,6 @@ with open('Open-Data-Gemeinderatswahl-Bayern1106.csv') as csvfile:
     data_rows = all_rows[1:]
 
 places = [row[4] for row in data_rows]
-briefwahlIndices = [index for index, location in enumerate(places) if 'Briefwahl' in location]
-realPlaces = [place for index, place in enumerate(places) if index not in briefwahlIndices]
 
 for index, columName in enumerate(first_row):
     if columName.lower() in spd_kandidaten:
@@ -33,23 +31,4 @@ for index, columName in enumerate(first_row):
         for i, v in enumerate(votes):
             ax.text(v, i-.3, str(v))
 
-        plot.savefig('output/'+candidateName+' mit Briefwahl')
-
-        votesWithoutBriefwahl = [vote for index, vote in enumerate(votes) if index not in briefwahlIndices]
-
-        plot.clf()
-
-        plot.rcdefaults()
-        fig, ax = plot.subplots(figsize=(8, 5))
-
-        ax.barh(realPlaces, votesWithoutBriefwahl)
-        ax.set_xlabel('Stimmen')
-        ax.set_title('{} ({} Stimmen ohne Briefwahl)'.format(candidateName,sum(votesWithoutBriefwahl)))
-        fig.subplots_adjust(left=0.3)
-
-        for i, v in enumerate(votesWithoutBriefwahl):
-            ax.text(v, i-.3, str(v))
-
-        plot.savefig('output/'+candidateName+' ohne Briefwahl')
-
-
+        plot.savefig('output/'+candidateName)
